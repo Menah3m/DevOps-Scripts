5e0dbce103e97d6d41fe48cc4469ad647cc3937d
yasu <568200065@qq.com>
2020-08-21 21:55:50 +0900

    更新 一键更新 v1

---
+++ auto-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author: menah3m
+# Date: 20200821
+# Description: patch update 
+
+yum -y upgrade;
+
+yum -y update > /tmp/$(date +%Y%m%d%H%M%S).log;
+echo "update success!!"
+~                                                                                                                          
+~                                                                                                                          
+~                                                                                                                          
+~                                                                                                                          
+~ 
